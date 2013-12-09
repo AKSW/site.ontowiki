15c9272394a9d97fe59b1ee3ae85d5cc39eebace
Denis Kuchelev <kda@bandk.ru>
2013-12-09 05:58:36 +0400

    Change fetched template options priorities

--- TemplateOptionsClass.php
+++ TemplateOptionsClass.php
@@ -110,8 +110,7 @@ class TemplateOptionsClass
             $options = new TemplateOptionsClass($result[$other->getName()]);
             foreach ($options->_options as $arrayKey => $priorities) {
                 foreach ($priorities as $priority => $values) {
-                    $floor = floor($priority);
-                    $this->_options[$arrayKey][(string)($floor + 0.1*$this->_getPriority($result) + 0.1*($priority - $floor))] = $values;
+                    $this->_options[$arrayKey][(string)(static::PRIORITY_LAST + $this->_getPriority($result) + 0.1*$priority)] = $values;
                 }
             }
             foreach ($options->_optionLocalNames as $arrayKeyLocalName => $keys) {
