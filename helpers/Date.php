d7618f3c9779ab6b99c29f79296fec7035720b58
Natanael Arndt <arndtn@gmail.com>
2014-12-19 15:53:21 +0100

    Fix date handling for year month and month day

--- helpers/Date.php
+++ helpers/Date.php
@@ -24,13 +24,18 @@ class Site_View_Helper_Date extends Zend_View_Helper_Abstract
         }
         if (is_object($dateString) && $dateString instanceof Erfurt_Rdf_Literal) {
             if ($dateString->getDatatype() == 'http://www.w3.org/2001/XMLSchema#gYear') {
-                $formatString = 'Y';
+                $formatString = (string)$translate->_('DATE_FORMAT_YEAR');
+            } else if ($dateString->getDatatype() == 'http://www.w3.org/2001/XMLSchema#gYearMonth') {
+                $formatString = (string)$translate->_('DATE_FORMAT_YEARMONTH');
+            } else if ($dateString->getDatatype() == 'http://www.w3.org/2001/XMLSchema#gMonthDay') {
+                $formatString = (string)$translate->_('DATE_FORMAT_MONTHDAY');
             }
+
             $dateString = $dateString->getLabel();
         }
         $date = new Zend_Date();
         $date->setOptions(array('format_type' => 'php'));
-        $date->set($dateString);
+        $date->setDate($dateString, "yy-mm-j");
         $date->setLocale($translate->getLocale());
         return $date->toString($formatString);
     }
